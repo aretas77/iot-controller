241e30ab724de82521707b1799be3e097a5c2b77
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-05 19:18:24 +0200

    iotctl: models: add Node interface
    Node will be an interface to keep data about
    simple IoT edge devices.

---
+++ web/iotctl/database/models/node.go
@@ -0,0 +1,37 @@
+package models
+
+import "errors"
+
+var (
+	// ErrNodeNilPass ...
+	ErrNodeNilPass = errors.New("(nil) passed instead of (Node)")
+	// ErrNodeNotFound ...
+	ErrNodeNotFound = errors.New("(Device) not found")
+)
+
+type Node struct {
+	ID           string `json:"_key" db:"_key"`
+	Location     string `json:"location" db:"location"`
+	ReadInterval int    `json:"interval" db:"interval"`
+	IpAddress4   string `json:"ipv4" db:"ipv4"`
+	IpAddress6   string `json:"ipv6" db:"ipv6"`
+}
+
+type NodeService interface {
+
+	// Init will be used to initialize all needed information and migration
+	// data.
+	Init() error
+
+	// Create should create a Node device in the specified Database.
+	Create(nodeID string, n *Node) (*Node, error)
+
+	// Get should return a Node by given ID.
+	Get(nodeID string) (*Node, error)
+
+	// All should return all Nodes.
+	All() ([]Node, error)
+
+	// Update should update the given Node with given values.
+	Update(n *Node) (*Node, error)
+}
