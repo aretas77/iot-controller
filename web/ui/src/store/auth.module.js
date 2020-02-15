e79a6d0ce1c5424bd54132d39cdf4bd256ad2068
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 20:40:40 +0200

    ui: add auth store and return it

---
+++ web/ui/src/store/auth.module.js
@@ -0,0 +1,81 @@
+import ApiService from '@/common/api.service'
+import JwtService from '@/common/jwt.service'
+import {
+  LOGIN,
+  LOGOUT,
+  CHECK_AUTH
+} from './actions.type'
+import { SET_AUTH, PURGE_AUTH, SET_ERROR } from './mutations.type'
+
+const state = {
+  errors: null,
+  user: {},
+  isAuthenticated: !!JwtService.getToken()
+}
+
+const getters = {
+  currentUser (state) {
+    return state.user
+  },
+  isAuthenticated (state) {
+    return state.isAuthenticated
+  }
+}
+
+const actions = {
+  [LOGIN] (context, credentials) {
+    console.log('hello ' + credentials)
+    return new Promise(resolve => {
+      ApiService.post('users/login', { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user)
+          resolve(data)
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors)
+        })
+    })
+  },
+  [LOGOUT] (context) {
+    context.commit(PURGE_AUTH)
+  },
+  [CHECK_AUTH] (context) {
+    if (JwtService.getToken()) {
+      ApiService.setHeader()
+      ApiService.get('user')
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user)
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors)
+        })
+    } else {
+      context.commit(PURGE_AUTH)
+    }
+  }
+}
+
+const mutations = {
+  [SET_ERROR] (state, error) {
+    state.errors = error
+  },
+  [SET_AUTH] (state, user) {
+    state.isAuthenticated = true
+    state.user = user
+    state.errors = {}
+    JwtService.saveToken(state.user.token)
+  },
+  [PURGE_AUTH] (state) {
+    state.isAuthenticated = false
+    state.user = {}
+    state.errors = {}
+    JwtService.destroyToken()
+  }
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
--- web/ui/src/store/index.js
+++ web/ui/src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import node from './node.module'
+import auth from './auth.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
+    auth,
     node
   }
 })
