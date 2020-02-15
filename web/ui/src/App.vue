2a315e4bc3b89bb90363e523d09a141b29005e9f
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 20:08:27 +0200

    ui: export Header into a component

--- web/ui/src/App.vue
+++ web/ui/src/App.vue
@@ -1,48 +1,19 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="md" type="dark" variant="dark">
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-        <b-navbar-brand to="/">IoT Controller</b-navbar-brand>
-        <b-collapse is-nav id="nav_collapse">
-            <b-navbar-nav>
-                <b-nav-item to="/">Home</b-nav-item>
-                <b-nav-item to="/nodes-manager">Nodes</b-nav-item>
-                <b-nav-item to="/models">Models</b-nav-item>
-                <b-nav-item href="#" @click.prevent="login" v-if="!activeUser">Login</b-nav-item>
-                <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar>
-    <router-view/>
+    <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default {
   name: 'app',
-  data () {
-    return {
-      activeUser: null
-    }
-  },
-  async created () {
-    await this.refreshActiveUser()
-  },
-  watch: {
-    // everytime a route is changed refresh the activeUser
-    $route: 'refreshActiveUser'
-  },
-  methods: {
-    login () {
-      this.$auth.loginRedirect()
-    },
-    async refreshActiveUser () {
-    },
-    async logout () {
-      await this.refreshActiveUser()
-      this.$router.push('/')
-    }
+  components: {
+    Header,
+    Footer
   }
 }
 
@@ -55,18 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ web/ui/src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-navbar class="fixed" toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand to="/">IoT Controller</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/nodes-manager">Nodes</b-nav-item>
+          <b-nav-item to="/models">Models</b-nav-item>
+          <b-nav-item href="#" @click.prevent="login" v-if="!activeUser">Login</b-nav-item>
+          <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Header',
+  computed: {
+    ...mapGetters(['currentUser', 'isAuthenticated'])
+  },
+  data () {
+    return {
+      activeUser: null
+    }
+  },
+  async created () {
+    await this.refreshActiveUser()
+  },
+  watch: {
+    // everytime a route is changed refresh the activeUser
+    $route: 'refreshActiveUser'
+  },
+  methods: {
+    login () {
+      this.$auth.loginRedirect()
+    },
+    async refreshActiveUser () {
+    },
+    async logout () {
+      await this.refreshActiveUser()
+      this.$router.push('/')
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #3c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
