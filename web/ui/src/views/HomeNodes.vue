1cfa0877500532ab7b611a97050bd741238253a3
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 22:48:44 +0200

    ui: add HomeNodes view

---
+++ web/ui/src/views/HomeNodes.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-nodes">
+    <NodeList type="all" />
+  </div>
+</template>
+
+<script>
+import NodeList from '@/components/NodeList'
+
+export default {
+  name: 'home-nodes',
+  components: {
+    NodeList
+  }
+}
+</script>
