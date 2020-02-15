1f7b383b4846fa2b56d48346a2ba6d5405c4e50a
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 20:08:45 +0200

    ui: add mutation commands

---
+++ web/ui/src/store/mutations.type.js
@@ -0,0 +1,8 @@
+export const FETCH_START = 'setLoading'
+export const SET_NODE = 'setNode'
+export const SET_NODES = 'setNodes'
+export const PURGE_AUTH = 'logOut'
+export const SET_ERROR = 'setError'
+export const UPDATE_NODE_IN_LIST = 'updateNodeInList'
+export const RESET_MODULE = 'resetModuleState'
+export const RESET_STATE = 'resetState'
