03ba9714fab0fe26e4a2c7c6b6f8e53b71694874
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 15:51:09 +0200

    ui: add Footer component

---
+++ web/ui/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="fixed-bottom footer">
+    <div class="container">
+      <span class="attribution">
+        IoT Controller
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped lang="scss">
+.footer {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  background-color: #f5f5f5;
+}
+</style>
