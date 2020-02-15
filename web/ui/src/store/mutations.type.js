c1359de21a2a6e8fc572dbb49e19413d56ff9ce2
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-15 20:41:15 +0200

    ui: update action and mutation types

--- web/ui/src/store/mutations.type.js
+++ web/ui/src/store/mutations.type.js
@@ -1,6 +1,7 @@
 export const FETCH_START = 'setLoading'
 export const SET_NODE = 'setNode'
 export const SET_NODES = 'setNodes'
+export const SET_AUTH = 'setAuth'
 export const PURGE_AUTH = 'logOut'
 export const SET_ERROR = 'setError'
 export const UPDATE_NODE_IN_LIST = 'updateNodeInList'
