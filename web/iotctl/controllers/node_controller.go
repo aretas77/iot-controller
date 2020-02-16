ae1cb6e00295055b8436721f12bc4f5270423403
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-16 21:10:59 +0200

    iotctl: add new methods for node controller
    Added method barebones:
     - GetNodes
     - GetNode
     - RegisterNode
     - UnregisterNode

--- web/iotctl/controllers/node_controller.go
+++ web/iotctl/controllers/node_controller.go
@@ -25,15 +25,42 @@ func (n *NodeController) setupHeader(w *http.ResponseWriter) {
 		"POST, GET, OPTIONS, DELETE, PATCH")
 	(*w).Header().Set("Access-Control-Allow-Headers",
 		"Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, Access-Control-Allow-Origin")
-
 }
 
+// GetNode should return a Node by its ID.
 func (n *NodeController) GetNode(w http.ResponseWriter, r *http.Request,
 	next http.HandlerFunc) {
 
 	vars := mux.Vars(r)
+
+	query := "SELECT * FROM nodes"
+
+	n.Database.GetMySql().Query(query)
+
 	fmt.Println(vars)
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(nil)
+}
+
+func (n *NodeController) GetNodes(w http.ResponseWriter, r *http.Request,
+	next http.HandlerFunc) {
+
+	query := "SELECT * FROM nodes"
+	n.Database.GetMySql().Query(query)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(nil)
+}
+
+// RegisterNode should add the Node to the specified network.
+func (n *NodeController) RegisterNode(w http.ResponseWriter, r *http.Request,
+	next http.HandlerFunc) {
+
+}
+
+// UnRegisterNode should remove the Node from our network.
+func (n *NodeController) UnregisterNode(w http.ResponseWriter, r *http.Request,
+	next http.HandlerFunc) {
+
 }
