bf764fde31e1043f684232c8d3788f112927af37
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-18 19:36:36 +0200

    iotctl: add new routes for nodes

--- web/iotctl/routers/node.go
+++ web/iotctl/routers/node.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/aretas77/iot-controller/web/iotctl/controllers"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 )
 
@@ -10,18 +11,21 @@ import (
 // manipulation
 func SetNodeRoutes(router *mux.Router, ctl *controllers.ApiController) *mux.Router {
 
+	logrus.Debugf("Attaching negroni handler on GET %s", "/nodes")
 	router.Handle(
 		"/nodes",
 		negroni.New(
 			negroni.HandlerFunc(ctl.NodeCtl.GetNode),
 		)).Methods("GET")
 
+	logrus.Debugf("Attaching negroni handler on GET %s", "/nodes/{id}")
 	router.Handle(
 		"/nodes/{id}",
 		negroni.New(
 			nil,
 		)).Methods("GET")
 
+	logrus.Debugf("Attaching negroni handler on POST %s", "/nodes")
 	router.Handle(
 		"/nodes",
 		negroni.New(
