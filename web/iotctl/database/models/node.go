3f0d6ab64133e6c72b1521bb014edd1394443eb3
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-18 19:37:32 +0200

    iotctl: add new fields for Node model

--- web/iotctl/database/models/node.go
+++ web/iotctl/database/models/node.go
@@ -12,13 +12,26 @@ var (
 	ErrNodeNotFound = errors.New("(Device) not found")
 )
 
+type Status string
+
+const (
+	Acknowledged = "acknowledged"
+	Registered   = "registered"
+	Unregistered = "unregistered"
+)
+
 type Node struct {
 	ID           int       `json:"_key" db:"_key"`
+	Name         string    `json:"name" db:"name"`
+	Mac          string    `json:"mac" db:"mac"`
+	Serial       string    `json:"serial" db:"serial"`
 	Location     string    `json:"location" db:"location"`
 	SendInterval int       `json:"send_interval" db:"send_interval"`
 	IpAddress4   string    `json:"ipv4" db:"ipv4"`
 	IpAddress6   string    `json:"ipv6" db:"ipv6"`
 	LastUpdated  time.Time `json:"last_update" db:"last_update"`
+	LastSentAck  time.Time `json:"last_sent_ack" db:"last_sent_ack"`
+	Status       Status    `json:"status" sql:"type:ENUM('acknowledged', 'registered', 'unregistered')" gorm:"default:'acknowledged'"`
 }
 
 type NodeService interface {
