5def05357d7d28f6c2138ecbce7e231313cc0c0f
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-26 23:33:26 +0200

    iotctl: split mysql to mysql and mysql_gorm

--- web/iotctl/database/mysql/mysql.go
+++ web/iotctl/database/mysql/mysql.go
@@ -33,24 +33,6 @@ type MySql struct {
 	GormDb *gorm.DB
 }
 
-func (m *MySql) ConnectGorm() (err error) {
-	logrus.Debug("Setting up MySQL database using GORM")
-
-	m.GormDb, err = gorm.Open("mysql", m.Server)
-	if err != nil {
-		logrus.Error(ErrOpenFailed)
-		panic(err.Error())
-	}
-
-	//m.GormDb.LogMode(true)
-
-	// Get the generic database object sql.DB to use its functions
-	m.Db = m.GormDb.DB()
-
-	logrus.Infof("Connected to MySQL at %s", m.Server)
-	return
-}
-
 // Connect should connect to a MySQL database using database/sql driver
 // and gets the handle to the database.
 func (m *MySql) Connect() (err error) {
@@ -74,21 +56,6 @@ func (m *MySql) Connect() (err error) {
 
 	logrus.Infof("Connected to MySQL at %s", m.Server)
 	return
-}
-
-// InitializeMigrationGorm will create a database structure so it would be
-// possible to manipulate data with it.
-func (m *MySql) InitializeMigrationGorm() {
-	m.GormDb.DropTableIfExists(&models.Network{}, &models.User{},
-		&models.Node{}, &models.NodeSettings{}, &models.UnregisteredNode{})
-
-	m.GormDb.CreateTable(&models.User{}, &models.Node{}, &models.NodeSettings{},
-		&models.UnregisteredNode{}, &models.Network{})
-
-	m.GormDb.Model(&models.Node{}).AddForeignKey("settings_id",
-		"node_settings(id)", "RESTRICT", "RESTRICT")
-	m.GormDb.Model(&models.Network{}).AddForeignKey("user_refer", "users(id)",
-		"RESTRICT", "RESTRICT")
 }
 
 func (m *MySql) Query(query string, args ...interface{}) error {
--- web/iotctl/database/mysql/mysql_gorm.go
+++ web/iotctl/database/mysql/mysql_gorm.go
@@ -1,1 +1,40 @@
 package mysql
+
+import (
+	models "github.com/aretas77/iot-controller/web/iotctl/database/models"
+	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
+)
+
+func (m *MySql) ConnectGorm() (err error) {
+	logrus.Debug("Setting up MySQL database using GORM")
+
+	m.GormDb, err = gorm.Open("mysql", m.Server)
+	if err != nil {
+		logrus.Error(ErrOpenFailed)
+		panic(err.Error())
+	}
+
+	//m.GormDb.LogMode(true)
+
+	// Get the generic database object sql.DB to use its functions
+	m.Db = m.GormDb.DB()
+
+	logrus.Infof("Connected to MySQL at %s", m.Server)
+	return
+}
+
+// InitializeMigrationGorm will create a database structure so it would be
+// possible to manipulate data with it.
+func (m *MySql) InitializeMigrationGorm() {
+	m.GormDb.DropTableIfExists(&models.Network{}, &models.User{},
+		&models.Node{}, &models.NodeSettings{}, &models.UnregisteredNode{})
+
+	m.GormDb.CreateTable(&models.User{}, &models.Node{}, &models.NodeSettings{},
+		&models.UnregisteredNode{}, &models.Network{})
+
+	m.GormDb.Model(&models.Node{}).AddForeignKey("settings_id",
+		"node_settings(id)", "RESTRICT", "RESTRICT")
+	m.GormDb.Model(&models.Network{}).AddForeignKey("user_refer", "users(id)",
+		"RESTRICT", "RESTRICT")
+}
