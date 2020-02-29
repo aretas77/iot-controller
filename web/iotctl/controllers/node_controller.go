53a3f852a7e46562af11cfd4e792f52c5e7af88c
Aretas Paulauskas <aretas.pau@gmail.com>
2020-02-29 13:34:01 +0200

    iotctl: implement a proper AddNode method
    Added a logic which adds UnregisteredNode into database
    if no Node exists yet, otherwise, update status for a Node
    entry in the database 'acknowledged' -> 'registered'.

--- web/iotctl/controllers/node_controller.go
+++ web/iotctl/controllers/node_controller.go
@@ -3,12 +3,14 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 	"time"
 
 	db "github.com/aretas77/iot-controller/web/iotctl/database"
 	models "github.com/aretas77/iot-controller/web/iotctl/database/models"
 	mysql "github.com/aretas77/iot-controller/web/iotctl/database/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 )
 
@@ -43,25 +45,53 @@ func (n *NodeController) migrateNodeGorm() error {
 	settings := &models.NodeSettings{
 		ReadInterval: 10,
 		SendInterval: 10,
+		NodeID:       2,
+	}
+
+	settings2 := &models.NodeSettings{
+		ReadInterval: 10,
+		SendInterval: 20,
+		NodeID:       1,
 	}
 
 	if n.sql.GormDb.NewRecord(settings) {
 		n.sql.GormDb.Create(&settings)
 	}
 
+	if n.sql.GormDb.NewRecord(settings2) {
+		n.sql.GormDb.Create(&settings2)
+	}
+
 	node := &models.Node{
-		Name:        "TestNode",
-		Mac:         "AA:BB:CC:DD:EE",
-		Location:    "Kaunas",
-		IpAddress4:  "172.8.0.20",
-		IpAddress6:  "NA",
-		LastSentAck: time.Now(),
-		Status:      "acknowledged",
-		SettingsID:  settings.ID,
+		Name:         "TestNode",
+		Mac:          "AA:BB:CC:DD:EE",
+		Location:     "Kaunas",
+		IpAddress4:   "172.8.0.20",
+		IpAddress6:   "NA",
+		LastSentAck:  time.Now(),
+		Status:       "acknowledged",
+		SettingsID:   settings2.ID,
+		NetworkRefer: 1,
+	}
+
+	node2 := &models.Node{
+		Name:         "TestNode2",
+		Mac:          "AA:BB:CC:DD:EF",
+		Location:     "Kaunas",
+		IpAddress4:   "172.8.0.21",
+		IpAddress6:   "NA",
+		LastSentAck:  time.Now(),
+		Status:       "acknowledged",
+		SettingsID:   settings.ID,
+		NetworkRefer: 1,
 	}
 
 	if n.sql.GormDb.NewRecord(node) {
 		n.sql.GormDb.Create(&node)
+	}
+
+	if n.sql.GormDb.NewRecord(node2) {
+		n.sql.GormDb.Create(&node2)
 	}
 
 	return nil
@@ -109,29 +139,86 @@ func (n *NodeController) GetNodes(w http.ResponseWriter, r *http.Request,
 		n.sql.GormDb.Where("id = ?", node.NetworkRefer).First(&network)
 
 		nodes[i].Settings = settings
-		nodes[i].Network = network
+		nodes[i].Network = &network
 	}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(nodes)
 }
 
+// AddNode will parse `UnregisteredNode` from the request and check it against
+// database to see if such a Node already exists and if it exists - it will
+// register the `Node` and won't create an entry for `UnregisteredNode`.
+//
+// Otherwise, the `UnregisteredNode` will be added to the database and will point
+// to the `Network` it was created and won't point to any of the `Node`s.
 func (n *NodeController) AddNode(w http.ResponseWriter, r *http.Request,
 	next http.HandlerFunc) {
 
-	// Add node to unregistered nodes
 	n.setupHeader(&w)
 	decoder := json.NewDecoder(r.Body)
 
-	logrus.Debugf("got request")
-	var tmpNode models.Node
+	var tmpNode models.UnregisteredNode
 	if err := decoder.Decode(&tmpNode); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	} else {
+		// MAC address was not specified - don't continue.
+		if strings.Compare("", tmpNode.Mac) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		// Check if a Node with current MAC address exists.
-		n.sql.GormDb.FirstOrCreate(&tmpNode, models.Node{Mac: tmpNode.Mac})
-		w.WriteHeader(http.StatusCreated)
+		node := models.Node{}
+		err := n.sql.GormDb.Where("mac = ?", tmpNode.Mac).Find(&node).Error
+		if err != nil && !gorm.IsRecordNotFoundError(err) {
+			logrus.Error(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// The Node exists and its status is Registered - ignore this request.
+		if node.Status != "" && node.Status == models.Registered {
+			logrus.Infof("A Node(ID = %d) is already %s", node.ID, node.Status)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Node exists and is ready for registration - change Node status and
+		// required fields.
+		if node.Status == models.Acknowledged {
+			logrus.Infof("A Node(ID = %d) is %s and is ready for registration",
+				node.ID, node.Status)
+			logrus.Infof("Update Node(ID = %d) status to %s", node.ID,
+				models.Registered)
+
+			err = n.sql.GormDb.Model(&node).Update("status", models.Registered).Error
+			if err != nil {
+				logrus.Error(err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Node doesn't exist yet - add UnregisteredNode into a List for future
+		// requests.
+		err = n.sql.GormDb.Where(models.UnregisteredNode{Mac: tmpNode.Mac}).FirstOrCreate(&tmpNode).Error
+		if err != nil {
+			logrus.Error(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 	}
+
+	// If there are no existing `Node` with the same MAC address as an
+	// `UnregisteredNode` then a new `UnregisteredNode` entry is created in
+	// a database and thus StatusCreated is returned.
+	w.WriteHeader(http.StatusCreated)
 }
 
 // RegisterNode should add the Node to the specified network.
