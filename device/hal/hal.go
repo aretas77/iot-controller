e1ecaec8a396b3d95a930c1a74894a0b3450da41
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-03 22:23:47 +0200

    device: add hal interface

---
+++ device/hal/hal.go
@@ -0,0 +1,55 @@
+package hal
+
+const (
+	// Radio chip is on. The chip can receive, transmit or listen.
+	ActiveMode = "active"
+
+	// The CPU is operational and the clock is configurable. WiFi/Bluetooth
+	// are disabled.
+	ModemSleepMode = "modemSleep"
+
+	// The CPU is paused. Any wake-up event will wake the device.
+	LightSleepMode = "lightSleep"
+
+	//
+	DeepSleepMode = "deepSleep"
+)
+
+// PowerConsumption structure defines what can consume power and will
+// specify how much.
+// All values are in mA (mili-Amperes).
+type PowerConsumption struct {
+	Tx80211b   int // Transmit 802.11b
+	Tx80211g   int // Transmit 802.11g
+	Tx80211n   int // Transmit 802.11n
+	Rx80211bgn int // Receive 802.11b/g/n
+	TxBt       int // Transmit BT/BLE
+	RxBt       int // Receive BT/BLE
+}
+
+// HAL is short for Hardware Abstraction Layer and its used as a reference
+// to various devices with `built-in` sensors.
+// TODO: `built-in` sensors should become as modules which could be dynamically
+// attached to a device.
+type HAL interface {
+
+	// GetDeviceName will return the name of the device.
+	GetDeviceName() string
+
+	// GetInterface will return the name of the interface e.g. esp32.
+	GetInterface() string
+
+	// GetTemperature will return a temperature value and how much energy was
+	// used by using the sensor.
+	GetTemperature() (float32, float32)
+
+	// SetPowerMode will set the power mode of the simulation device which
+	// will adjust TX, RX and other peripheral power consumption.
+	SetPowerMode(mode string) error
+
+	// GetPowerMode will return the devices operating mode.
+	GetPowerMode() (string, error)
+
+	// GetBatteryPercentage will return the battery left percentage.
+	GetBatteryPercentage() float32
+}
