a06a20337f0971204f1a6bc4cbff329dd090bfe0
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-05 20:13:25 +0200

    device: add initial esp32 HAL implementation

---
+++ device/hal/esp32.go
@@ -0,0 +1,92 @@
+package hal
+
+import "errors"
+
+const (
+	// Radio chip is on. The chip can receive, transmit or listen.
+	ActiveMode = "active"
+
+	// The CPU is operational and the clock is configurable. WiFi/Bluetooth
+	// are disabled.
+	ModemSleepMode = "modemSleep"
+
+	// The CPU is paused. Any wake-up event will wake the device.
+	LightSleepMode = "lightSleep"
+
+	//
+	DeepSleepMode = "deepSleep"
+)
+
+var (
+	ErrModeInvalid = errors.New("mode doesn't exist")
+)
+
+type ESP32 struct {
+	Name      string // Name of the device using this interface
+	Interface string // Name of this struct
+
+	// TODO: make PowerConsumption dependent on Mode.
+	Power   *PowerConsumption
+	Mode    string
+	Battery int
+}
+
+func (e *ESP32) Initialize(name string) error {
+	e = &ESP32{
+		Name: name,
+		Power: &PowerConsumption{
+			// All values in mA, taken from ESP32 datasheet.
+			// https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf, page 42.
+			Tx80211b:   240,
+			Tx80211g:   190,
+			Tx80211n:   180,
+			Rx80211bgn: 100,
+			TxBt:       130,
+			RxBt:       100,
+		},
+		Mode:      ActiveMode,
+		Interface: "esp32",
+		// Battery at 100%.
+		Battery: 100,
+	}
+
+	return nil
+}
+
+func (e *ESP32) GetDeviceName() string {
+	return e.Name
+}
+
+func (e *ESP32) GetInterface() string {
+	return e.Interface
+}
+
+func (e *ESP32) GetTemperature() (float32, float32) {
+	return 0, 0
+}
+
+func (e *ESP32) SetPowerMode(mode string) error {
+	switch mode {
+	case ActiveMode:
+		e.Mode = ActiveMode
+	case ModemSleepMode:
+		e.Mode = ModemSleepMode
+	case LightSleepMode:
+		e.Mode = LightSleepMode
+	case DeepSleepMode:
+		e.Mode = DeepSleepMode
+	default:
+		return ErrModeInvalid
+	}
+
+	return nil
+}
+
+func (e *ESP32) GetPowerMode() string {
+	return e.Mode
+}
+
+func (e *ESP32) GetBatteryPercentage() float32 {
+
+	return 0
+}
