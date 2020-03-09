41a414202404c6b349a76875f54c292581bafc4a
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-09 22:08:33 +0200

    device: export topic subscription to subscribe.go

---
+++ device/subscribe.go
@@ -0,0 +1,36 @@
+package device
+
+import (
+	"time"
+
+	"github.com/aretas77/iot-controller/types/mqtt"
+	"github.com/sirupsen/logrus"
+)
+
+func (d *DeviceController) subscribeDevicePlainTopics() error {
+	d.PlainTopics = []mqtt.TopicHandler{
+		{"control/+/+/ack", d.MessageHandler},
+		{"test", d.MessageHandler},
+	}
+
+	for _, t := range d.PlainTopics {
+		token := d.Plain.Client.Subscribe(t.Topic, 0, t.Handler)
+		if token.WaitTimeout(3 * time.Second); token.Error() != nil {
+			return token.Error()
+		}
+		logrus.Debugf("plain subscribed topic %s", t.Topic)
+	}
+
+	return nil
+}
+
+func (d *DeviceController) unsubscribeDevicePlainTopics() {
+	if !d.Plain.Client.IsConnected() {
+		return
+	}
+
+	for _, t := range d.PlainTopics {
+		d.Plain.Client.Unsubscribe(t.Topic)
+		logrus.Debugf("plain unsubscribing: %s", t.Topic)
+	}
+}
