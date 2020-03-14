e63f7d41943d9a11adb10a69e34b762c7af9c467
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-15 00:56:29 +0200

    ui: add Node action component

---
+++ web/ui/src/components/NodeActions.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Used when User is also owner of the Node or Admin -->
+  <span v-if="canModify">
+
+    <button class="btn btn-outline-danger btn-sm" @click="deleteNode">
+      <i class="ion-trash-a"></i> <span>&nbsp;Delete Node</span>
+    </button>
+  </span>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import {
+  NODE_REMOVE
+} from '@/store/actions.type'
+
+export default {
+  name: 'IotctlNodeActions',
+  props: {
+    node: {
+      type: Object,
+      required: true
+    },
+    canModify: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  computed: {
+    ...mapGetters(['profile', 'isAuthenticated']),
+    editNodeLink () {
+      return { name: 'node-edit', params: { slug: this.node.slug } }
+    }
+  },
+
+  methods: {
+    async deleteNode () {
+      try {
+        await this.$store.dispatch(NODE_REMOVE, this.node.slug)
+        this.$router.push('/')
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+
+</script>
