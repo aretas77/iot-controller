ec3df73ae4e4fe8add46dbeb22243c2b6dfc250e
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-15 00:56:14 +0200

    ui: add error lists and filter

---
+++ web/ui/src/components/ListErrors.vue
@@ -0,0 +1,20 @@
+<template v-show="errors">
+  <ul class="error-messages">
+    <li v-for="(value, key) in errors" :key="key">
+      <span v-text="key" />
+      <span v-for="err in value" :key="err" v-text="err" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'IotctlListErrors',
+  props: {
+    errors: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
