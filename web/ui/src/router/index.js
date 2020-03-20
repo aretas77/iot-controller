b66168aee0e20397357011b0d497da7bc730ddd3
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-20 21:32:46 +0200

    ui: remove not needed views

--- web/ui/src/router/index.js
+++ web/ui/src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
       {
         path: '/@:username/profile',
         name: 'profile',
-        component: () => import('@/views/ProfileNodes')
+        component: null
       }
     ]
   },
