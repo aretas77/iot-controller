3ae5858ea4d6cc7d65d8b7ff08051ebe7a7d6e9d
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-22 22:01:52 +0200

    ui: add color to battery level indicator

--- web/ui/src/components/VNodePreview.vue
+++ web/ui/src/components/VNodePreview.vue
@@ -1,15 +1,21 @@
 <template>
   <b-container fluid class="node-preview-container">
-    <b-row class="m-3 node-preview-row ">
+    <b-row fluid class="m-3 node-preview-row">
 
-      <b-col cols=8 class="d-flex align-items-start">
+      <b-col cols="2" class="d-flex align-items-start">
         <router-link :to="nodeLink" class="preview-link">
           <h3 v-text="node.name" />
           <p v-text="node.mac" />
         </router-link>
       </b-col>
 
-      <b-col cols=4>
+      <b-col cols="2" class="text-left">
+        <span class="date">Last active: {{ node.last_received | time }}</span>
+        <br />
+        <span class="battery" v-bind:class="activeColor">Battery: {{ node.battery_left_per }}% </span>
+      </b-col>
+
+      <b-col offset="6">
         <NodeMeta isPreview :node="node" :actions="true" />
       </b-col>
 
@@ -26,6 +32,13 @@ export default {
   components: {
     NodeMeta
   },
+  data () {
+    return {
+      good: true,
+      average: false,
+      bad: false
+    }
+  },
   props: {
     node: { type: Object, required: true }
   },
@@ -36,6 +49,26 @@ export default {
         params: {
           slug: this.node.ID
         }
+      }
+    },
+    activeColor: function () {
+      return {
+        'battery-good': this.good && !this.average && !this.bad,
+        'battery-average': !this.good && this.average && !this.bad,
+        'battery-bad': !this.good && !this.average && this.bad
+      }
+    }
+  },
+  watch: {
+    node () {
+      if (this.node.battery_left_per >= 80) {
+        this.good = true
+        this.bad = false
+        this.average = false
+      } else if (this.node.battery_left_per < 80) {
+        this.good = false
+        this.average = false
+        this.bad = true
       }
     }
   }
@@ -51,5 +84,15 @@ export default {
 }
 
 .preview-link {
+}
+
+.battery-bad {
+  color: red;
+}
+.battery-good {
+  color: green;
+}
+.batter-average {
+  color: yellow;
 }
 </style>
