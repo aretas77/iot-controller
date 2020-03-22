999d97fda48ee2713ec7c616eccb91aa278d3b08
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-22 14:15:34 +0200

    types: add FreeNetworkScheduler for System

--- types/devices/system.go
+++ types/devices/system.go
@@ -3,14 +3,15 @@ package devices
 // System is the configuration of the whole system and is periodically sent
 // to the server.
 type System struct {
-	Mac               string  `json:"mac"`
-	Name              string  `json:"name"`
-	Location          string  `json:"location"`
-	Network           string  `json:"network"`
-	IpAddress4        string  `json:"ip_address4"`
-	Status            string  `json:"status"`
-	BatteryPercentage float32 `json:"battery_left_per"`
-	BatteryMah        float32 `json:"battery_left_mah"`
+	Mac                  string               `json:"mac"`
+	Name                 string               `json:"name"`
+	Location             string               `json:"location"`
+	Network              string               `json:"network"`
+	IpAddress4           string               `json:"ip_address4"`
+	Status               string               `json:"status"`
+	BatteryPercentage    float32              `json:"battery_left_per"`
+	BatteryMah           float32              `json:"battery_left_mah"`
+	FreeNetworkScheduler FreeNetworkScheduler `json:"free_net_scheduler"`
 }
 
 type FreeNetworkScheduler struct {
