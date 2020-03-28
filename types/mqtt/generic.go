482ec23e991e299e3dd90a753ab67b44c8ecbdd2
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-28 23:01:52 +0200

    types: add MQTTClient interface

--- types/mqtt/generic.go
+++ types/mqtt/generic.go
@@ -1,14 +1,41 @@
 package mqtt
 
-import MQTT "github.com/eclipse/paho.mqtt.golang"
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 // MQTTConnection will represent a single MQTT connection with its options.
 type MQTTConnection struct {
-	Options *MQTT.ClientOptions
-	Client  MQTT.Client
+	Options *mqtt.ClientOptions
+	Client  mqtt.Client
 }
 
 type TopicHandler struct {
 	Topic   string
-	Handler func(c MQTT.Client, msg MQTT.Message)
+	Handler func(c mqtt.Client, msg mqtt.Message)
+}
+
+// MessageHandler acts as an overlay to the MessageHandler of any mqtt lib.
+type MessageHandler func(c mqtt.Client, msg mqtt.Message)
+
+// MQTTClient interface represents an underlying MQTT client implementation
+// without regard to the used MQTT client library.
+type MQTTClient interface {
+	// IsConnected should return whether the client is connected to the broker.
+	IsConnected() bool
+
+	// Connect should connect to the MQTT broker.
+	Connect() error
+
+	// Disconnect should disconnect from the MQTT broker.
+	Disconnect() error
+
+	// Publish should publish the given payload to the given topic with a given QoS.
+	Publish(topic string, qos uint8, payload interface{}) error
+
+	// Subscribe should subscribe to the given topic with a given QoS and message handler.
+	Subscribe(topic string, qos uint8, callback MessageHandler) error
+
+	// Unsubscribe should unsubscribe from the given topic.
+	Unsubscribe(topic string) error
 }
