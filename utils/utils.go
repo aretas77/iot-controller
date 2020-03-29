f956223b036223304dee2397237c1d177bb2e944
Aretas Paulauskas <aretas.pau@gmail.com>
2020-03-29 14:10:33 +0300

    utils: add read data split utility

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "strings"
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
 
 func StripBearerPrefixFromTokenString(tok string) (string, error) {
 	// Should be a bearer token
@@ -13,4 +17,25 @@ func StripBearerPrefixFromTokenString(tok string) (string, error) {
 func SplitTopic4(topic string) (string, string, string, string) {
 	tok := strings.Split(topic, "/")
 	return tok[0], tok[1], tok[2], tok[3]
+}
+
+// SplitDataReadLine splits a read data line from the sensor data.
+func SplitDataReadLine(line string) (error, string, float64, float64, float64) {
+	tok := strings.Split(line, ";")
+
+	var err error
+	var temperature, pressure, meters float64
+	if temperature, err = strconv.ParseFloat(tok[1], 32); err != nil {
+		return errors.New("failed to parse temperature"), tok[0], 0, 0, 0
+	}
+
+	if pressure, err = strconv.ParseFloat(tok[2], 32); err != nil {
+		return errors.New("failed to parse pressure"), tok[0], 0, 0, 0
+	}
+
+	if meters, err = strconv.ParseFloat(tok[3], 32); err != nil {
+		return errors.New("failed to parse meters"), tok[0], 0, 0, 0
+	}
+
+	return nil, tok[0], temperature, pressure, meters
 }
