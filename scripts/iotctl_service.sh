3d1494b13c2901f34066dec18f23c51a4114af1c
Aretas Paulauskas <aretas.pau@gmail.com>
2020-04-09 13:55:26 +0300

    update script for mqtt mocking

--- scripts/iotctl_service.sh
+++ scripts/iotctl_service.sh
@@ -56,11 +56,45 @@ send_model_request() {
 
     reqTopic="hades/global/${mac}/model/request"
 
+	# construct a sample request - a request could also be empty.
 cat > ${tmp}/${requestFile} << EOL
 { "mac": ${mac}" }
 EOL
 
+	# possible to send a null message - '--null-mesage'
 	mosquitto_pub -u mock -P test -h "$server" -p "$port" -t "$reqTopic" -f "$tmp"/"$requestFile"
+}
+
+# send_model_file is used to send a file (in our case - a model) to the device.
+# A sent model should be received and processed by paho.mqtt library and NOT by device.
+# The file will be sent 
+# $1 - MAC
+# $2 - filename
+send_model_file() {
+	mac=$1
+	file=$2
+
+	if [ -z "$mac" ] || [ -z "$file" ]; then
+		printf "MAC or File is empty.\n"
+		printf "MAC	 = %s.\n"
+		printf "File = %s.\n"
+	fi
+
+	sendTopic="node/global/${mac}/hades/model/receive"
+
+	mosquitto_pub -u mock -P test -h "$server" -p "$port" -t "$sendTopic"
+}
+
+# print_usage will print some information on how to use this script.
+print_usage() {
+cat <<EOF
+Usage: $0 [options]
+
+send_ack		| {MAC}				this will send ack to IoT Controller.
+send_stats		| {MAC}				this will send a mock statistic entry to IoT controller.
+send_model_request	| {MAC}				this will send a new request for model to the Hades.
+send_model_file		| {MAC} {FILE}			this will send a file as bytes to paho.mqtt library.
+EOF
 }
 
 case "$1" in
@@ -73,10 +107,11 @@ case "$1" in
     send_model_request)
         send_model_request "$2"
         ;;
+	send_model_file)
+		send_model_file "$2" "$3"
+		;;
     *)
-        if [ -z "$1" ]; then
-            echo "Empty args, usage: [send_ack {MAC}] [send_stats {MAC}] [send_model_request {MAC}]"
-        fi
         echo "Received argument: $1"
+        [ -z "$1" ] && print_usage
         exit 1
 esac
