7516f2bfc3bbfd20a1bd645769f7a11630d2c611
Aretas Paulauskas <aretas.pau@gmail.com>
2020-04-12 14:59:38 +0300

    iotctl: some changes for event model

--- web/iotctl/database/models/event.go
+++ web/iotctl/database/models/event.go
@@ -1,9 +1,10 @@
 package models
 
-// Event ...
+// Event represents a singular event of a model being sent by Hades to the
+// `NodeDevice`.
 type Event struct {
 	ID       uint   `json:"id" gorm:"primary_key"`
 	Model    string `json:"model"`
 	TimeSent string `json:"time_sent"`
-	Mac      string `json:"mac"`
+	Mac      string `json:"mac" gorm:"not null`
 }
